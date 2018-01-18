legend-exp/legendexp_legend-base_img

Initial container configuration

provisioning/install-sw-scripts/clhep-setup.sh
@@ -1,0 +1,32 @@
+# This software is licensed under the MIT "Expat" License.
+#
+# Copyright (c) 2016: Oliver Schulz.
+
+
+DEFAULT_BUILD_OPTS=""
+
+
+pkg_install() {
+    DOWNLOAD_URL="http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-${PACKAGE_VERSION}.tgz"
+    echo "INFO: Download URL: \"${DOWNLOAD_URL}\"." >&2
+
+    mkdir src build
+    download "${DOWNLOAD_URL}" | tar --strip-components 1 -C src --strip=1 -x -z
+    cd build
+
+    cmake \
+        -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+        ${DEFAULT_BUILD_OPTS} \
+        ../src/CLHEP
+
+    time make "-j${NTHREADS}" install
+}
+
+
+pkg_env_vars() {
+cat <<-EOF
+PATH="${INSTALL_PREFIX}/bin:\$PATH"
+LD_LIBRARY_PATH="${INSTALL_PREFIX}/lib:\$LD_LIBRARY_PATH"
+export PATH LD_LIBRARY_PATH
+EOF
+}

jgruszko/g4simple_img

provisioning/install-sw-scripts/clhep-setup.sh
@@ -1,0 +1,32 @@
+# This software is licensed under the MIT "Expat" License.
+#
+# Copyright (c) 2016: Oliver Schulz.
+
+
+DEFAULT_BUILD_OPTS=""
+
+
+pkg_install() {
+    DOWNLOAD_URL="https://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-${PACKAGE_VERSION}.tgz"
+    echo "INFO: Download URL: \"${DOWNLOAD_URL}\"." >&2
+
+    mkdir src build
+    download "${DOWNLOAD_URL}" | tar --strip-components 1 -C src --strip=1 -x -z
+    cd build
+
+    cmake \
+        -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+        ${DEFAULT_BUILD_OPTS} \
+        ../src/CLHEP
+
+    time make -j"$(nproc)" install
+}
+
+
+pkg_env_vars() {
+cat <<-EOF
+PATH="${INSTALL_PREFIX}/bin:\$PATH"
+LD_LIBRARY_PATH="${INSTALL_PREFIX}/lib:\$LD_LIBRARY_PATH"
+export PATH LD_LIBRARY_PATH
+EOF
+}